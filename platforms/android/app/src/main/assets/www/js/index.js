4ba08d584276cda917916e901db8b6b20cabf564
Richard Haim <s50711@edu.campus02.at>
2024-01-16 17:43:07 +0100

    online/offline-abfrage

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -2,6 +2,14 @@ window.addEventListener("DOMContentLoaded", (event) => {
     readJsonObjFromFile();
     });
 
+window.addEventListener('load', function(e) {
+    if (navigator.onLine) {
+        console.log('We\'re online!');
+    } else {
+        console.log('We\'re offline...');
+    }
+}, false);
+
 document.getElementById('welcome').addEventListener('click', function() {
     document.location.href = 'home.html'
 });
