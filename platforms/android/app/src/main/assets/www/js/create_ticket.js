7d2cf163128d6a7217aee80c3b2d55c4d596aba2
Richard Haim <s50711@edu.campus02.at>
2024-01-19 14:35:34 +0100

    when entering new ticket, the date is pushed automatically without selection. Removed selection option

--- platforms/android/app/src/main/assets/www/js/create_ticket.js
+++ platforms/android/app/src/main/assets/www/js/create_ticket.js
@@ -1,7 +1,7 @@
 import * as common from './common.js';
 
 window.onload = function () {
-    datetime_input.value = common.getCurrentDateTime();
+    //datetime_input.value = common.getCurrentDateTime();
     fillUpDropdowns();
 };
 
@@ -26,23 +26,13 @@ function getFieldInput () {
         'SupportTeamId': document.getElementById("SupportTeamId_names").value,
         'StatusTicketId': 1, //document.getElementById("TicketStatusId_names").value,
         'Beschreibung': document.getElementById("description_input").value,
-        'DatumEingabe': document.getElementById("datetime_input").value
+        'DatumEingabe': common.getCurrentDateTime() //document.getElementById("datetime_input").value
         };
     return payload;
 };
 
-/*
-function getCurrentDateTime() {
-    var now = new Date();
-    var year = now.getFullYear();
-    var month = (now.getMonth() + 1).toString().padStart(2, '0');
-    var day = now.getDate().toString().padStart(2, '0');
-    var formattedDateTime = `${year}-${month}-${day}`;
-    return formattedDateTime;
- };
-*/
- function fillUpDropdowns () {
+function fillUpDropdowns () {
     common.fillDropdown('problemkategorie', 'problem_names');
     common.fillDropdown('dringlichkeit', 'dringlichkeitid_names');
     common.fillDropdown('supportteam', 'SupportTeamId_names');
- }
+}
