743d64abd86203cc5ef5c76ce0d603582ba1a7d9
Richard Haim <s50711@edu.campus02.at>
2024-01-21 18:16:02 +0100

    tickets created offline are stored in localstorage and automatically pushed when app is opened & online

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -4,6 +4,16 @@ import * as common from './common.js';
 // wenn online UND initial -> lade daten vom server + speichere in localstorage
 window.addEventListener('load', async function() {
     showLoadingPopup(true); // anzeigen des load pop ups wenn daten laden
+
+    // push and clear newTicketQUEUE if not empty:
+    const online = await common.onlinechecker();
+    const queueNotEmpty = localStorage.getItem('newTicketsQUEUE');
+    if ( queueNotEmpty !== null && online ) {
+        const payload = common.readJsonObjFromFile('newTicketsQUEUE');
+        payload.forEach(item => {
+            common.pushticket(item); });
+        localStorage.removeItem('newTicketsQUEUE');
+    };
     await common.serverLoad();
     showLoadingPopup(false); // verstecken des load pop ups wenn daten geladen wurden
     common.fillDropdown('mitarbeiter', 'mitarbeiterDropdown');
